2c39fe4dc0c2fe55c41a9f2dbe36d63b93ac4e36
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-06-16 15:35:12 -0500

    6. test: testing if a mock has been called at least once

--- test/easy-example.test.js
+++ test/easy-example.test.js
@@ -4,4 +4,22 @@ describe("Suite tests to an easy example of mocking", () => {
         const mockCalledBack = jest.fn();
         expect(mockCalledBack.getMockName()).toBe("jest.fn()");
     });
+
+    it("2. Verifying if a mock created has called at least once", () => {
+        const mockCalledBack2 = jest.fn((x,y,z) => (x*z)+y);
+        mockCalledBack2(2,5,6);
+        mockCalledBack2(3,4,9);
+
+        /*
+            Arreglo con llamadas al mock, la cual lucirá así:
+            [
+                [...args] <- primera llamada
+                [...args] <- segunda llamada
+                ...
+                ...
+                [...args] <- última llamada
+            ]
+        */
+        expect(mockCalledBack2.mock.calls.length).toBe(1);
+    })
 });
