9d4e01c97bcebe7ba2606ed48dd3c2330e9b2868
MikoVelascoRamirez <launchx08269@innovaccion.mx>
2022-06-16 15:58:32 -0500

    7. test: testing if the calls values results are the correct

--- test/easy-example.test.js
+++ test/easy-example.test.js
@@ -9,6 +9,9 @@ describe("Suite tests to an easy example of mocking", () => {
         const mockCalledBack2 = jest.fn((x,y,z) => (x*z)+y);
         mockCalledBack2(2,5,6);
         mockCalledBack2(3,4,9);
+        mockCalledBack2();
+
+        console.log(mockCalledBack2.mock.results);
 
         /*
             Arreglo con llamadas al mock, la cual lucirá así:
@@ -22,4 +25,19 @@ describe("Suite tests to an easy example of mocking", () => {
         */
         expect(mockCalledBack2.mock.calls.length).toBe(1);
     })
+
+    it("3. Verifying if the values returned are the correct", () => {
+        const mockCalledBack3 = jest.fn(name => {
+            const splited =  name.split("");
+            return splited[0] + splited[splited.length-1];
+        })
+
+        mockCalledBack3("Miguelito")
+        mockCalledBack3("Elizabeth")
+        mockCalledBack3("Zaira")
+
+        expect(mockCalledBack3.mock.results[0].value).toBe("Mo")
+        expect(mockCalledBack3.mock.results[1].value).toBe("Eh")
+        expect(mockCalledBack3.mock.results[2].value).toBe("Za")
+    });
 });
